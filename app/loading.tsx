fe2534b226176843702d41757e28bae6df9649cd
loystonpais <loyston500@gmail.com>
2024-12-04 10:05:03 +0530

    initial commit

---
+++ app/loading.tsx
@@ -0,0 +1,11 @@
+import { Progress } from "@/components/ui/progress";
+
+const Loading = ()=> {
+    return (
+        <>
+        <Progress value={50}></Progress>
+        </>
+    )
+}
+
+export default Loading;
