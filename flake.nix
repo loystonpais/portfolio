fd6d1c17aa507ea74ab6499b5ea49375f969f15e
loystonpais <loyston500@gmail.com>
2024-12-10 14:04:43 +0530

    feat: added nixos module containing service to run the website

--- flake.nix
+++ flake.nix
@@ -17,9 +17,45 @@
         default = import ./shell.nix { pkgs = nixpkgsFor.${system}; };
       });
 
+      nixosModules.default = { config, lib, pkgs, ... }: {
+        options.programs = {
+          portfolio-website = {
+            enable = lib.mkEnableOption "enables portfolio site";
+            port = lib.mkOption {
+              type = lib.types.int;
+              description = "Port to run the site on";
+              default = 3001;
+            };
+          };
+        };
+
+        config = lib.mkIf config.programs.portfolio-website.enable {
+          systemd.services.portfolio-website = 
+          let 
+            pkg = pkgs.callPackage ./default.nix { inherit pkgs; };
+            name = (builtins.fromJSON ( builtins.readFile ./package.json )).name;
+          in
+          {
+            enable = true;
+
+            serviceConfig = {
+              ExecStart = 
+                "${pkg}/bin/${name} -p ${builtins.toString config.programs.portfolio-website.port}";
+              Restart = "on-failure";
+              TimeoutStartSec = 5;
+              RestartSec = 2;
+            };
+
+            wantedBy = [ "multi-user.target" ];
+
+          };
+        };
+      };
+
     in {
       inherit packages;
       inherit devShells;
+      inherit nixosModules;
     };
 
 }
