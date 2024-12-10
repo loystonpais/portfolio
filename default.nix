ff4158b9727f23dac4af9b6e48f81c40b91b6c59
loystonpais <loyston500@gmail.com>
2024-12-10 13:00:38 +0530

    feat: improved derivation, supports file exclusion

--- default.nix
+++ default.nix
@@ -1,9 +1,56 @@
 { pkgs ? import <nixpkgs> { } }:
+with builtins;
+let 
+  packageConfig = fromJSON ( readFile ./package.json );
+
+  srcDir = with pkgs.lib.fileset; toSource {
+    root = ./.;
+    fileset =
+    let
+      # IIRC cleanSource removes .git
+      cleaned = fromSource (pkgs.lib.cleanSource ./.);
+
+      nixFiles = fileFilter (f: f.hasExt "nix") ./.;
+
+      gitignores = fileFilter (f: f.name == ".gitignore") ./.;
+
+      other = unions ( map maybeMissing [ 
+        ./result 
+        ./.direnv
+        ./.github 
+        ./.idea
+        ./.envrc
+        
+        ./flake.lock
+
+        ./node_modules
+        ./.next 
+        
+        ./README.md
+      ] );
+
+      fileset = difference cleaned ( unions [ 
+        nixFiles 
+        gitignores 
+        other 
+      ] ); 
+    in trace fileset fileset;
+  };
+in
+
 pkgs.buildNpmPackage rec {
-  version = "v0.0.0.test";
-  pname = "portfolio";
-  src = ./.;
-  npmDepsHash = "sha256-Cl7bgcZ3Xp762jvRvFsCvKvLsgX6dAeVhecza8Ebs8E=";
+  version = packageConfig.version;
+  pname = packageConfig.name;
+  src = srcDir;
+  
+  npmDepsHash = "sha256-pQXPbZsR4uJvArAafShoNuzm+qhKWtsmgoHTUxyNUfY=";
+
+  npmInstallFlags = ["--force"];
+
+  makeCacheWritable = true;
+
+  npmFlags = [ "--legacy-peer-deps" ];
+
   postInstall = ''
     mkdir -p $out/bin
     exe="$out/bin/${pname}"
@@ -14,6 +61,6 @@ pkgs.buildNpmPackage rec {
     echo "
         #!/usr/bin/env bash
         cd $lib
-        ${pkgs.nodePackages_latest.pnpm}/bin/pnpm run start" > $exe
+        ${pkgs.nodePackages_latest.pnpm}/bin/pnpm run start \$@" > $exe
   '';
 }
